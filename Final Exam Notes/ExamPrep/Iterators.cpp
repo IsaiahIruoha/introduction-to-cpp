173d1902435b9324baed7e0b57717a949a3de92b
Isaiah Iruoha <Siruoha@gmail.com>
2024-12-15 22:49:26 -0500

    updated code

--- Final Exam Notes/ExamPrep/Iterators.cpp
+++ Final Exam Notes/ExamPrep/Iterators.cpp
@@ -8,11 +8,17 @@ int main () {
     
     vector<string> planets = {"Mercury", "Venus", "Earth", "Mars", "Neptune"}; 
 
+    int array[6]{1,2,3,4,5,6}; 
+
     // for(int i = 0; i < planets.size(); i++) {
     //     cout << planets[i] << endl; 
     // }
 
     /// ranged based for loops 
+
+    for(auto element : array) {
+        cout << element << endl; 
+    }
 
     for(vector<string>::iterator it = planets.begin(); it != planets.end(); ++it)
         cout <<*it << endl; 
---
+++ Final Exam Notes/ExamPrep/Vectors.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std; 
+
+int main () {
+
+    vector<int> v1 = {1, 2, 3}; 
+
+    cout << v1.size() << endl; 
+
+
+    return 0; 
+}
